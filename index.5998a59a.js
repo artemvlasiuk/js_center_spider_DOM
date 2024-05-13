03f021032af5258bd204f88e9d1ddfdb30557475
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-05-14 02:04:08 +0300

    make build

---
+++ index.5998a59a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");Object.assign(document.querySelector(".spider").style,{position:"static"}),Object.assign(e.style,{display:"flex",justifyContent:"center",alignItems:"center"});
+//# sourceMappingURL=index.5998a59a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+Object.assign(spider.style, {
+    position: "static"
+});
+Object.assign(wall.style, {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
